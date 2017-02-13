60ee78a898c8b621917ca77d3125652f1b8839f6
shafeeqonline <shafeeq.rahman01@gmail.com>
2017-02-13 17:34:00 +0530

    Created prod webpack config and pushed distribution folder

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router';
-import {browserHistory} from 'react-router';
+import { Link } from 'react-router';
+import { browserHistory } from 'react-router';
 import './search-bar.scss';
 import debounce from 'lodash.debounce';
 import SearchDropDown from './../../containers/search-drop-down-container';
-var aql = require('jfrog-aql');
-export default class SearchBar extends Component{
-	constructor(props){
-		super(props)
-		this.searchInputTitle = null;
-		this.searchFunction = debounce(this.searchFunction.bind(this),500);
-	}
-	searchFunction(value){
-		let input = this.searchInputTitle.value;
-		/*if(value){
-			this.props.searchSuggestion(true);
-			this.props.updateSearchValue(value);
-			
-		}*/
-		/*var username = "shafeeq",
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+        this.searchInputTitle = null;
+        this.searchFunction = debounce(this.searchFunction.bind(this), 500);
+    }
+    searchFunction(value) {
+        let input = this.searchInputTitle.value;
+        /*if(value){
+        	this.props.searchSuggestion(true);
+        	this.props.updateSearchValue(value);
+        	
+        }*/
+        /*var username = "shafeeq",
 			password = "password",
 			auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
 		  // config object is used by the Request class
@@ -33,43 +32,52 @@ export default class SearchBar extends Component{
     		aql.query(aqlQuery).then((data) => {
         	console.log(data);
     		});*/
-				
-		
-	}	
-	onClickOut(){
-		this.props.searchSuggestion(false);
-	}
-	handleKeyDown(e,value){
-		let numOptions = this.props.searchResults.length;
-		
-		switch(e.keyCode){
 
-	      case 38: // up arrow
-	        if (this.props.activeIndex>0) {
-	          this.props.handleDownArrow( this.props.activeIndex-1,this.props.searchResults[this.props.activeIndex].value);
-	        } else {
-	          this.props.handleDownArrow(numOptions-1,this.props.searchResults[this.props.activeIndex].value);
-	        }
-	        break;
 
-	      case 40: // down arrow
-	      	let activeIndex=(this.props.activeIndex+1) % numOptions;
-	        this.props.handleDownArrow(activeIndex,this.props.searchResults[activeIndex].value);
-	        break;
-	      case 13:
-	      	this.searchFunction(false);
-	        browserHistory.push('/details')
-	        break;
-	    }
-	}
-	render(){
-		return(
-		<div className = 'search-bar'>
-				<input className = 'search-input' placeholder = 'Search' ref={(el) => {this.searchInputTitle = el;}} onClick = {this.searchFunction.bind(this)} onKeyDown = {this.handleKeyDown.bind(this)}/>
-				<Link to='/results' className='search-router'>
-					<span className = 'search-icon fa fa-search'></span>
-				</Link>	
-		</div>
-		)
-	}
+    }
+    onClickOut() {
+        this.props.searchSuggestion(false);
+    }
+    handleKeyDown(e, value) {
+        let numOptions = this.props.searchResults.length;
+
+        switch (e.keyCode) {
+
+            case 38: // up arrow
+                if (this.props.activeIndex > 0) {
+                    this.props.handleDownArrow(this.props.activeIndex - 1, this.props.searchResults[this.props.activeIndex].value);
+                } else {
+                    this.props.handleDownArrow(numOptions - 1, this.props.searchResults[this.props.activeIndex].value);
+                }
+                break;
+
+            case 40: // down arrow
+                let activeIndex = (this.props.activeIndex + 1) % numOptions;
+                this.props.handleDownArrow(activeIndex, this.props.searchResults[activeIndex].value);
+                break;
+            case 13:
+                this.searchFunction(false);
+                browserHistory.push('/details')
+                break;
+        }
+    }
+    render() {
+        return ( <
+            div className = 'search-bar' >
+            <
+            input className = 'search-input'
+            placeholder = 'Search'
+            ref = {
+                (el) => { this.searchInputTitle = el; } }
+            onClick = { this.searchFunction.bind(this) }
+            onKeyDown = { this.handleKeyDown.bind(this) }
+            /> <
+            Link to = '/results'
+            className = 'search-router' >
+            <
+            span className = 'search-icon fa fa-search' > < /span> <
+            /Link>	 <
+            /div>
+        )
+    }
 }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,38 +1,65 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/template.html',
+    filename: 'index.html',
+    inject: 'body'
+});
 
 module.exports = {
-  devtool: 'source-map',
-  entry: [
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: ['babel'],
-        include: path.join(__dirname, 'src')
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style!css!sass'
-      }
+    devtool: 'source-map',
+    entry: [
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'js/bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    ],
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                exclude: [/node_modules/, /styles/],
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+            },
+            {
+                test: /\.(jpg|png)$/,
+                loader: 'file?name=images/[name].[ext]'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file?name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("css/bundle.css"),
+        HtmlWebpackPluginConfig
     ]
-  }
 };
